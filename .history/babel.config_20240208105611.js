8b405413727007e12c93c346b6e24c045502bc3d
RossDee <diying@sjtu.edu.cn>
2024-02-08 11:17:14 +0800

    reorgnize the file structure

---
+++ .history/babel.config_20240208105611.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins": [
+  ["module-resolver", {
+  "alias": {
+    "components": "./src/components",
+    "constants": "./src/constants",
+    "assets": "./src/app/assets"
+  }
+}]
+]
+  }
+}
---
+++ .history/babel.config_20240208105615.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins": [
+  ["module-resolver", {
+  "alias": {
+    "components": "./src/components",
+    "constants": "./src/constants",
+    "assets": "./src/app/assets"
+  }
+}]
+  }
+}
